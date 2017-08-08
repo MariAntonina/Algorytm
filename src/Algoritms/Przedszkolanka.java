4f45997addd5387fc1bf56690736e158d96f92e1
MariAntonina <maryhuzarek@gmail.com>
2017-08-08 15:54:55 +0200

    yep

--- src/Algoritms/Przedszkolanka.java
+++ src/Algoritms/Przedszkolanka.java
@@ -14,8 +14,15 @@ public class Przedszkolanka extends AbstractAlgoritm {
 
         int gr1 = Integer.parseInt(input[1]);
         int gr2 = Integer.parseInt(input[2]);
+        int iloczybGrup = gr1*gr2;
 
+        while (gr2 != 0){
+            int n = gr2;
+            gr2 = gr1%gr2;
+            gr1 = n;}
+            iloczybGrup = iloczybGrup/gr1;
+        System.out.println(iloczybGrup);
+        }
 
 
     }
-}
