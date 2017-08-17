86b30b3c37dfa51dcb339ea4018efcd8f469c795
MariAntonina <maryhuzarek@gmail.com>
2017-08-17 20:58:22 +0200

    koniec

--- src/ZbiorZadan/tagiHTMLowe.java
+++ src/ZbiorZadan/tagiHTMLowe.java
@@ -14,5 +14,29 @@ public class tagiHTMLowe extends AbstractAlgoritm {
     @Override
     public void runAlgoritm(String[] input) {
 
+        char[] word = new char[0];
+        for (int i = 1; i < input.length; i++) {
+            word = input[i].toCharArray();
+
+            for (int j = 0; j < word.length; j++) {
+                System.out.println();
+                if (word[j] == '<') {
+                    while (word[j] != '>') {
+                        System.out.print(Character.toString(word[j]).toUpperCase());
+                        j++;
+                    }
+                    System.out.print("> ");
+                } else {
+                    System.out.printf("%s", Character.toString(word[j]));
+                    if (j == word.length - 1) {
+                        System.out.print(" ");
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
+
